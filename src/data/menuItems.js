4958fbd3afa8e655c1be8d2f0e0c2f1018148523
Lukas Polak <lukash.polak@gmail.com>
2019-05-09 20:23:19 +0200

    create `menuItems` array

---
+++ src/data/menuItems.js
@@ -0,0 +1,37 @@
+export const menuItems = [
+  {
+    hidden: false,
+    label: 'splash-screen',
+    icon: 'bug',
+  },
+  {
+    hidden: false,
+    label: 'authentication',
+    icon: 'sign-in',
+  },
+  {
+    hidden: false,
+    label: 'home',
+    icon: 'home',
+  },
+  {
+    hidden: false,
+    label: 'my-profile',
+    icon: 'user',
+  },
+  {
+    hidden: false,
+    label: 'my-team',
+    icon: 'users',
+  },
+  {
+    hidden: false,
+    label: 'settings',
+    icon: 'cog',
+  },
+  {
+    hidden: false,
+    label: 'sign-out',
+    icon: 'sign-out',
+  },
+];
