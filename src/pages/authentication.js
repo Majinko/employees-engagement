749752a405b1a464d18396e4540a4bd2c642b8ba
Lukas Polak <lukash.polak@gmail.com>
2019-05-11 12:00:13 +0200

    wip `authentication.js` page

--- src/pages/authentication.js
+++ src/pages/authentication.js
@@ -4,12 +4,14 @@ import { Typography } from '@material-ui/core';
 
 import PaperCard from './../components/PaperCard';
 
+import AuthenticationTemplate from '../templates/AuthenticationTemplate';
 
 export default class AuthenticationPage extends React.Component {
   render() {
     return (
       <PaperCard>
         <Typography variant="h1">Authentication</Typography>
+        <AuthenticationTemplate />
       </PaperCard>
     );
   }
