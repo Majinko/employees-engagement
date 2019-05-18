f4815ecf252cd5bff4b738e22eb1af74711618b6
Lukas Polak <lukash.polak@gmail.com>
2019-05-18 12:42:24 +0200

    create new page - give feedback

---
+++ src/pages/give-feedback.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Typography } from '@material-ui/core';
+
+import PaperCard from './../components/PaperCard';
+import CustomGrid from './../components/CustomGrid';
+
+export default class GiveFeedbackPage extends React.Component {
+  render() {
+    return (
+      <CustomGrid>
+        <PaperCard>
+          <Typography variant="h1">Give Feedback</Typography>
+        </PaperCard>
+      </CustomGrid>
+    );
+  }
+}
