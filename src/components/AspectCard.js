700d5dbfdfd7787145b6efe101ab875be47c20cd
Lukas Polak <lukash.polak@gmail.com>
2019-05-19 11:16:12 +0200

    wip AspectCard componnet

--- src/components/AspectCard.js
+++ src/components/AspectCard.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import { connect } from 'react-redux';
+
+import { createPost } from '../actions/postActions';
 
 function createStyled(styles, options) {
   function Styled(props) {
@@ -32,22 +35,49 @@ const Styled = createStyled(theme => ({
   },
 }));
 
-function AspectCard({ children, aspectCard, ...props }) {
-  return (
-    <Styled>
-      {({ classes }) => (
-        <Paper
-          className={classes.aspectCard}
-          component={Button}
-          onClick={e => {
-            e.currentTarget.classList.toggle(classes.aspectCardActive);
-          }}
-        >
-          {children}
-        </Paper>
-      )}
-    </Styled>
-  );
+class AspectCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: '' };
+
+    this.ClickFn = this.ClickFn.bind(this);
+  }
+
+  ClickFn(e) {
+    e.currentTarget.classList.toggle('active');
+
+    const element = this.props.children[1].props.children;
+    this.setState({ title: element }, () => {
+      const post = {
+        title: this.state.title,
+      };
+      this.props.createPost(post);
+    });
+  }
+
+  render() {
+    return (
+      <Styled>
+        {({ classes }) => (
+          <Paper
+            className={classes.aspectCard}
+            component={Button}
+            onClick={this.ClickFn}
+            data-aspect={this.props.children[1].props.children}
+          >
+            {this.props.children}
+          </Paper>
+        )}
+      </Styled>
+    );
+  }
 }
 
-export default AspectCard;
+AspectCard.propTypes = {
+  createPost: PropTypes.func.isRequired,
+};
+
+export default connect(
+  null,
+  { createPost }
+)(AspectCard);
