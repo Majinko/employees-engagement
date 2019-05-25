28d5a7b5f05e6aa8a5286e45d8d6e7a76ade18bf
Lukas Polak <lukash.polak@gmail.com>
2019-05-25 16:41:36 +0200

    refactor `store.js` - fix firefox and smartphone bug

--- src/store.js
+++ src/store.js
@@ -3,17 +3,11 @@ import ReduxThunk from 'redux-thunk';
 
 import rootReducer from './reducers/index';
 
-const initialState = {};
-
-const middleware = [ReduxThunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(ReduxThunk))
 );
 
 export default store;
