c7021d39cf073b75a408315769c7f6dde9be3fd3
Lukas Polak <lukash.polak@gmail.com>
2019-06-09 19:42:29 +0200

    refactor papercard

--- src/components/PaperCard.js
+++ src/components/PaperCard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import { Link } from 'react-router-dom';
 
 const styles = theme => ({
   paper: {
@@ -14,7 +15,14 @@ const styles = theme => ({
 });
 
 function PaperCard({ children, ...props }) {
-  const { classes } = props;
+  const { classes, hasLink } = props;
+  if (hasLink) {
+    return (
+      <Link to="give-feedback">
+        <Paper className={classes.paper}>{children}</Paper>
+      </Link>
+    );
+  }
 
   return <Paper className={classes.paper}>{children}</Paper>;
 }
