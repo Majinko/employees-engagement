7e86c2fca15ac54fc1ecabdaed8c71ad42a4c079
Lukas Polak <lukash.polak@gmail.com>
2019-06-10 20:30:29 +0200

    quickfix

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -1,9 +1,10 @@
 export const setLoggedUser = () => dispatch => {
   let token = null;
 
-  if (localStorage.getItem('jwtToken'))
+  if (localStorage.getItem('jwtToken')) {
     token = JSON.parse(localStorage.getItem('jwtToken')).accessToken;
-  console.log('hello');
+  }
+
   dispatch({
     type: 'SET_LOGGED_USER',
     payload: { token },
