577eb81d17f007b8e8992db7dd91d6a154aa0546
Marek Hlavco <marek.hlavco@gmail.com>
2019-06-10 19:03:01 +0200

    change user service

--- src/services/user.js
+++ src/services/user.js
@@ -3,13 +3,13 @@ import { endpoint } from '../config';
 import { authHeader } from '../helpers/auth-header';
 import { handleResponse } from '../helpers/handle-response';
 
-export const userService = {
-  searchUser,
-};
-
 function searchUser(string) {
   const requestOptions = { method: 'GET', headers: authHeader() };
   return fetch(`${endpoint}/user/search?name=${string}`, requestOptions).then(
     handleResponse
   );
 }
+
+export const userService = {
+  searchUser,
+};
